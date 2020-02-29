a22bf4096550c30b16f72b90e009b2a2b24d49ac
ab <ytkonaft@gmail.com>
2020-02-29 19:43:34 +0200

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,220 +1,115 @@
-// generated on 2019-12-08 using generator-webapp 4.0.0-7
-const { src, dest, watch, series, parallel, lastRun } = require('gulp');
-const gulpLoadPlugins = require('gulp-load-plugins');
-const fs = require('fs');
-const mkdirp = require('mkdirp');
-const Modernizr = require('modernizr');
-const browserSync = require('browser-sync');
-const del = require('del');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const { argv } = require('yargs');
+var gulp           = require('gulp'),
+  gutil          = require('gulp-util' ),
+  sass           = require('gulp-sass'),
+  browserSync    = require('browser-sync'),
+  concat         = require('gulp-concat'),
+  uglify         = require('gulp-uglify'),
+  cleanCSS       = require('gulp-clean-css'),
+  rename         = require('gulp-rename'),
+  del            = require('del'),
+  imagemin       = require('gulp-imagemin'),
+  cache          = require('gulp-cache'),
+  autoprefixer   = require('gulp-autoprefixer'),
+  ftp            = require('vinyl-ftp'),
+  notify         = require("gulp-notify");
 
-const $ = gulpLoadPlugins();
-const server = browserSync.create();
+// Скрипты проекта
 
-const port = argv.port || 9000;
+gulp.task('common-js', function() {
+  return gulp.src([
+    'app/js/common.js',
+  ])
+    .pipe(concat('common.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
+});
 
-const isProd = process.env.NODE_ENV === 'production';
-const isTest = process.env.NODE_ENV === 'test';
-const isDev = !isProd && !isTest;
+gulp.task('scripts', ['common-js'], function() {
+  return gulp.src([
+    'app/libs/jquery/dist/jquery.min.js',
+    'app/libs/mmenu/js/jquery.mmenu.all.min.js',
+    'app/js/common.min.js', // Всегда в конце
+  ])
+    .pipe(concat('scripts.min.js'))
+    // .pipe(uglify()) // Минимизировать весь js (на выбор)
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({stream: true}));
+});
 
-function styles() {
-  return src('app/styles/*.scss')
-    .pipe($.plumber())
-    .pipe($.if(!isProd, $.sourcemaps.init()))
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.postcss([
-      autoprefixer()
-    ]))
-    .pipe($.if(!isProd, $.sourcemaps.write()))
-    .pipe(dest('.tmp/styles'))
-    .pipe(server.reload({stream: true}));
-};
-
-function scripts() {
-  return src('app/scripts/**/*.js')
-    .pipe($.plumber())
-    .pipe($.if(!isProd, $.sourcemaps.init()))
-    .pipe($.babel())
-    .pipe($.if(!isProd, $.sourcemaps.write('.')))
-    .pipe(dest('.tmp/scripts'))
-    .pipe(server.reload({stream: true}));
-};
-
-async function modernizr() {
-  const readConfig = () => new Promise((resolve, reject) => {
-    fs.readFile(`${__dirname}/modernizr.json`, 'utf8', (err, data) => {
-      if (err) reject(err);
-      resolve(JSON.parse(data));
-    })
-  })
-  const createDir = () => new Promise((resolve, reject) => {
-    mkdirp(`${__dirname}/.tmp/scripts`, err => {
-      if (err) reject(err);
-      resolve();
-    })
-  });
-  const generateScript = config => new Promise((resolve, reject) => {
-    Modernizr.build(config, content => {
-      fs.writeFile(`${__dirname}/.tmp/scripts/modernizr.js`, content, err => {
-        if (err) reject(err);
-        resolve(content);
-      });
-    })
-  });
-
-  const [config] = await Promise.all([
-    readConfig(),
-    createDir()
-  ]);
-  await generateScript(config);
-}
-
-const lintBase = files => {
-  return src(files)
-    .pipe($.eslint({ fix: true }))
-    .pipe(server.reload({stream: true, once: true}))
-    .pipe($.eslint.format())
-    .pipe($.if(!server.active, $.eslint.failAfterError()));
-}
-function lint() {
-  return lintBase('app/scripts/**/*.js')
-    .pipe(dest('app/scripts'));
-};
-function lintTest() {
-  return lintBase('test/spec/**/*.js')
-    .pipe(dest('test/spec'));
-};
-
-function html() {
-  return src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
-    .pipe($.if(/\.css$/, $.postcss([cssnano({safe: true, autoprefixer: false})])))
-    .pipe($.if(/\.html$/, $.htmlmin({
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: {compress: {drop_console: true}},
-      processConditionalComments: true,
-      removeComments: true,
-      removeEmptyAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true
-    })))
-    .pipe(dest('dist'));
-}
-
-function images() {
-  return src('app/images/**/*', { since: lastRun(images) })
-    .pipe($.imagemin())
-    .pipe(dest('dist/images'));
-};
-
-function fonts() {
-  return src('app/fonts/**/*.{eot,svg,ttf,woff,woff2}')
-    .pipe($.if(!isProd, dest('.tmp/fonts'), dest('dist/fonts')));
-};
-
-function extras() {
-  return src([
-    'app/*',
-    '!app/*.html'
-  ], {
-    dot: true
-  }).pipe(dest('dist'));
-};
-
-function clean() {
-  return del(['.tmp', 'dist'])
-}
-
-function measureSize() {
-  return src('dist/**/*')
-    .pipe($.size({title: 'build', gzip: true}));
-}
-
-const build = series(
-  clean,
-  parallel(
-    lint,
-    series(parallel(styles, scripts, modernizr), html),
-    images,
-    fonts,
-    extras
-  ),
-  measureSize
-);
-
-function startAppServer() {
-  server.init({
-    notify: false,
-    port,
+gulp.task('browser-sync', function() {
+  browserSync({
     server: {
-      baseDir: ['.tmp', 'app'],
-      routes: {
-        '/node_modules': 'node_modules'
-      }
-    }
+      baseDir: 'app'
+    },
+    notify: false,
+    // tunnel: true,
+    // tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
   });
+});
 
-  watch([
+gulp.task('scss', function() {
+  return gulp.src('app/styles/**/*.scss')
+    .pipe(sass().on("error", notify.onError()))
+    .pipe(rename({suffix: '.min', prefix : ''}))
+    .pipe(autoprefixer(['last 15 versions']))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['scss', 'scripts', 'browser-sync'], function() {
+  gulp.watch('app/styles/**/*.scss', ['scss']);
+  gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['scripts']);
+  gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('imagemin', function() {
+  return gulp.src('app/img/**/*')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', ['removedist', 'imagemin', 'scss', 'scripts'], function() {
+
+  var buildFiles = gulp.src([
     'app/*.html',
-    'app/images/**/*',
-    '.tmp/fonts/**/*'
-  ]).on('change', server.reload);
+    'app/.htaccess',
+  ]).pipe(gulp.dest('dist'));
 
-  watch('app/styles/**/*.scss', styles);
-  watch('app/scripts/**/*.js', scripts);
-  watch('modernizr.json', modernizr);
-  watch('app/fonts/**/*', fonts);
-}
+  var buildCss = gulp.src([
+    'app/css/main.min.css',
+  ]).pipe(gulp.dest('dist/css'));
 
-function startTestServer() {
-  server.init({
-    notify: false,
-    port,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/scripts': '.tmp/scripts',
-        '/node_modules': 'node_modules'
-      }
-    }
+  var buildJs = gulp.src([
+    'app/js/scripts.min.js',
+  ]).pipe(gulp.dest('dist/js'));
+
+  var buildFonts = gulp.src([
+    'app/fonts/**/*',
+  ]).pipe(gulp.dest('dist/fonts'));
+
+});
+
+gulp.task('deploy', function() {
+
+  var conn = ftp.create({
+    host:      'hostname.com',
+    user:      'username',
+    password:  'userpassword',
+    parallel:  10,
+    log: gutil.log
   });
 
-  watch('app/scripts/**/*.js', scripts);
-  watch(['test/spec/**/*.js', 'test/index.html']).on('change', server.reload);
-  watch('test/spec/**/*.js', lintTest);
-}
+  var globs = [
+    'dist/**',
+    'dist/.htaccess',
+  ];
+  return gulp.src(globs, {buffer: false})
+    .pipe(conn.dest('/path/to/folder/on/server'));
 
-function startDistServer() {
-  server.init({
-    notify: false,
-    port,
-    server: {
-      baseDir: 'dist',
-      routes: {
-        '/node_modules': 'node_modules'
-      }
-    }
-  });
-}
+});
 
-let serve;
-if (isDev) {
-  serve = series(clean, parallel(styles, scripts, modernizr, fonts), startAppServer);
-} else if (isTest) {
-  serve = series(clean, scripts, startTestServer);
-} else if (isProd) {
-  serve = series(build, startDistServer);
-}
+gulp.task('removedist', function() { return del.sync('dist'); });
+gulp.task('clearcache', function () { return cache.clearAll(); });
 
-exports.serve = serve;
-exports.build = build;
-exports.default = build;
+gulp.task('default', ['watch']);
